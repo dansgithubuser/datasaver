a568bd501bb805a8e8d064120fc4d7b4ced84c23
Dan <dansonlinepresence@gmail.com>
2019-02-02 19:10:34 -0500

    (ttc) prototype

--- datasaver/views.py
+++ datasaver/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def ttc(request):
+    return HttpResponse(loader.get_template('ttc.html').render({
+    }, request))
