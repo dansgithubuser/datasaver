92c119d034cae55146413847d19239a0d925114b
Dan <dansonlinepresence@gmail.com>
2019-02-03 20:31:09 -0500

    (ttc) only send nearby vehicles from backend to frontend

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 https://pacific-hamlet-93435.herokuapp.com/datasaver/ttc
 
 # todo
-- only send nearby vehicles from backend to frontend
 - make canvas interactive, to get extra info on vehicle
 - draw vehicle paths
--- datasaver/views.py
+++ datasaver/views.py
@@ -10,6 +10,21 @@ def ttc(request):
     }, request))
 
 def ttc_vehicles(request):
+    lat = float(request.GET['lat'])
+    lon = float(request.GET['lon'])
     r = urllib.request.urlopen('http://restbus.info/api/agencies/ttc/vehicles')
     j = json.loads(r.read())
-    return JsonResponse({'vehicles': j})
+    result = {}
+    for i in j:
+        if abs(i['lat'] - lat) + abs(i['lon'] - lon) > 5e-2: continue
+        result[i['id']] = dict(
+            lat=float(i['lat']),
+            lon=float(i['lon']),
+            heading=float(i['heading']),
+            routeId=i['routeId'],
+            directionId=i['directionId'],
+            predictable=i['predictable'],
+            leadingVehicleId=i['leadingVehicleId'],
+            secsSinceReport=float(i['secsSinceReport']),
+        )
+    return JsonResponse(result)
