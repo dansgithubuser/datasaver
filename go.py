b77bd7e5913ce34b349a227ef0c744f7c37facb3
Dan <dansonlinepresence@gmail.com>
2019-04-10 19:20:33 -0400

    update djangogo

--- go.py
+++ go.py
@@ -1,17 +1,17 @@
 import os
 import sys
 
-DIR=os.path.dirname(os.path.realpath(__file__))
+DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(DIR, 'deps'))
 
 import djangogo
 
-parser=djangogo.make_parser()
-args=parser.parse_args()
+parser = djangogo.make_parser()
+args = parser.parse_args()
 djangogo.main(args,
-	project='datasaver_proj',
-	app='datasaver',
-	database='datasaver_database',
-	user='datasaver_user',
-	heroku_url='https://dans-datasaver.herokuapp.com/',
+    project='datasaver_proj',
+    app='datasaver',
+    db_name='datasaver_database',
+    db_user='datasaver_user',
+    heroku_url='https://dans-datasaver.herokuapp.com/',
 )
